a6f8910682405dafce70fc21d7489a1b7280a5a1
dreamland-master pc <dreamland@onepatchdown.net>
2015-07-18 21:08:47 -0700

    dreamlandStructure: Hack debug mode.

--- dreamlandStructure.py
+++ dreamlandStructure.py
@@ -1,7 +1,7 @@
 from testConfig import structureConfig
 import pubSubDreamland as psdl
 # import gpioDreamland as ioDL
-import gpioDreamland as ioDL
+#import gpioDreamland as ioDL
 
 structureName = structureConfig["structureName"]
 serverIp = structureConfig["serverIp"]
@@ -12,19 +12,21 @@ send_port = '5559'
 sub = psdl.Subscriber(serverIp, recv_port, structureName, isServer=False)
 pub = psdl.Publisher(serverIp, send_port, structureName, isServer=False)
 
-outputPins = ioDL.stringToList(structureConfig['outputPins'])
+#outputPins = ioDL.stringToList(structureConfig['outputPins'])
+#
+#ioDL.setMode()
+#outputPins = [ioDL.Poofer(pin) for pin in outputPins]
 
-ioDL.setMode()
-outputPins = [ioDL.Poofer(pin) for pin in outputPins]
+print('name', structureName)
 
 while True:
 		poofer, command = sub.recvMessage().split()
 		if command == 'high':
 			print('using pin ' + poofer + ' high')
-			outputPins[int(poofer)].high()
+			#outputPins[int(poofer)].high()
 		elif command == 'low':
 			print('using pin ' + poofer + ' low')
-			outputPins[int(poofer)].low()
+			#outputPins[int(poofer)].low()
 		else:
 			print('WHAT YOU SAY?!')
 			pass
