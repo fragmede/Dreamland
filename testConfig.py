360821fe31a32873153d9965c423696c4cb5dcf6
dreamland-master pc <dreamland@onepatchdown.net>
2015-07-18 21:09:42 -0700

    testConfig: Hack testConfig.

--- testConfig.py
+++ testConfig.py
@@ -1,5 +1,5 @@
 structureConfig = {'structureName': 'carousel',
-                   'hostname': 'pi2.local',
-                   'serverIp': '192.168.2.45',
+                   'hostname': 'dreamland-master.local', #'pi2.local',
+                   'serverIp': '192.168.1.145', #'192.168.2.45',
                    'outputPins': '7, 11, 13, 15',
                    'inputPins': '[1, 2]'}
